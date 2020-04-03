fcb15af0b35244d0a83127be629cfd5d335ca18f
origin <315294970@qq.com>
2020-04-03 09:49:25 +0800

    Initial commit

--- eureka-provider-consumer/eureka/src/main/java/net/riking/springcloud/eureka/AppEureka.java
+++ eureka-provider-consumer/eureka/src/main/java/net/riking/springcloud/eureka/AppEureka.java
@@ -3,12 +3,32 @@ package net.riking.springcloud.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.locks.ReentrantLock;
+
 @EnableEurekaServer
 @SpringBootApplication
 public class AppEureka {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppEureka.class, args);
+
+		List<String>  list =new LinkedList<>();
+
+		Map<String,String> map=	new HashMap<>();
+		System.out.println(1 << 4);
+		map.put("xiali", "li");
+
+		ReentrantLock reentrantLock=new ReentrantLock();
+		reentrantLock.lock();
+
+
+		ConcurrentLinkedDeque<String> concurrentLinkedDeque=  new ConcurrentLinkedDeque<>();
+		concurrentLinkedDeque.offer("");
+
+		new HashSet<>();
+		//SpringApplication.run(AppEureka.class, args);
 	}
 
 }
--- ribbon-provider-consumer/ribbon-provider/src/main/java/net/riking/springcloud/provider/AppProvider.java
+++ ribbon-provider-consumer/ribbon-provider/src/main/java/net/riking/springcloud/provider/AppProvider.java
@@ -5,9 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootApplication
 @EnableEurekaClient  ///开启对EurekaClient的支持，即：作为Eureka客户端，高版本可省略
 public class AppProvider {
+    private static final ThreadLocal<String> threadCtx = new ThreadLocal<>();
     public static void main(String[] args) {
         SpringApplication.run(AppProvider.class, args);
     }
